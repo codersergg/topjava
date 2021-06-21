8aad3ab85b3d60a966d1dc433b0c1f0b8ff285d2
CoderSergg <ShopinSergg@gmail.com>
2021-06-21 22:24:13 +0300

    HW03 working on bugs

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -29,12 +29,11 @@ CREATE TABLE user_roles
 
 CREATE TABLE meals
 (
-    id          INTEGER PRIMARY KEY DEFAULT nextval ('meals_seq'),
-    date_time   TIMESTAMP,
-    description VARCHAR                             NOT NULL,
-    calories    INTEGER                             NOT NULL,
-    user_id     INTEGER                             NOT NULL
-        CONSTRAINT meals_users_id_fk
-            REFERENCES users
+    id          INTEGER DEFAULT nextval('meals_seq'),
+    date_time   TIMESTAMP NOT NULL,
+    description VARCHAR   NOT NULL,
+    calories    INTEGER   NOT NULL,
+    user_id     INTEGER   NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX meals_date_time_idx ON meals (date_time, user_id);
+CREATE UNIQUE INDEX meals_user_id_date_time_idx ON meals (user_id, date_time);
