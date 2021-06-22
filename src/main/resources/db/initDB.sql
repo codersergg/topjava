b93ff49f450fe9e00eda3d8ee44a4e6f92fdb4ed
CoderSergg <ShopinSergg@gmail.com>
2021-06-22 10:29:22 +0300

    HW03 deleted meals_seq

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -2,10 +2,8 @@ DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
-DROP SEQUENCE IF EXISTS meals_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
-CREATE SEQUENCE meals_seq START WITH 100;
 
 CREATE TABLE users
 (
@@ -29,7 +27,7 @@ CREATE TABLE user_roles
 
 CREATE TABLE meals
 (
-    id          INTEGER DEFAULT nextval('meals_seq'),
+    id          INTEGER DEFAULT nextval('global_seq'),
     date_time   TIMESTAMP NOT NULL,
     description VARCHAR   NOT NULL,
     calories    INTEGER   NOT NULL,
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -2,7 +2,6 @@ DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
-ALTER SEQUENCE meals_seq RESTART WITH 100;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
