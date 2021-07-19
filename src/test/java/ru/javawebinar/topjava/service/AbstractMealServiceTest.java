0e69a8e99bd8f1c6bfd313664fbdc2b7f7dd6fd4
CoderSergg <ShopinSergg@gmail.com>
2021-07-20 00:03:26 +0300

    1.2. Fix JDBC MealTest

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -1,15 +1,19 @@
 package ru.javawebinar.topjava.service;
 
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.DataAccessException;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.validation.ConstraintViolationException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Arrays;
 
 import static java.time.LocalDateTime.of;
 import static org.junit.Assert.assertThrows;
@@ -21,6 +25,9 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 
     @Autowired
     protected MealService service;
+
+    @Autowired
+    public Environment environment;
 
     @Test
     public void delete() {
@@ -103,8 +110,10 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
         MATCHER.assertMatch(service.getBetweenInclusive(null, null, USER_ID), meals);
     }
 
+
     @Test
     public void createWithException() throws Exception {
+        Assume.assumeTrue(Arrays.stream(environment.getActiveProfiles()).anyMatch(prof -> Profiles.JPA.equals(prof) || Profiles.DATAJPA.equals(prof)));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "  ", 300), USER_ID));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new Meal(null, null, "Description", 300), USER_ID));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description", 9), USER_ID));
